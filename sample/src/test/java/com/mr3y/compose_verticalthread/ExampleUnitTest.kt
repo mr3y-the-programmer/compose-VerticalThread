a05988145f4cd2b20523d16e8c930464db7fa7df
MR3Y <abdonasr379@gmail.com>
2022-02-15 14:35:57 +0200

    Configure ktlint & explicit api mode

--- sample/src/test/java/com/mr3y/compose_verticalthread/ExampleUnitTest.kt
+++ sample/src/test/java/com/mr3y/compose_verticalthread/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.mr3y.compose_verticalthread
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- vertical-thread/src/androidTest/java/com/mr3y/vertical_thread/ExampleInstrumentedTest.kt
+++ vertical-thread/src/androidTest/java/com/mr3y/vertical_thread/ExampleInstrumentedTest.kt
@@ -1,24 +1,22 @@
 package com.mr3y.vertical_thread
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.mr3y.vertical_thread.test", appContext.packageName)
     }
 }
